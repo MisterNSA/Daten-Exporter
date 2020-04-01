2bd5c445b4d43b8addf43ee2117eea5242b9cfbd
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-04-01 15:31:13 +0200

    Anfang der Mailfunktion
    Beschreibung geupdatet und mit Mailfunktion angefangen.

--- Export_functions.py
+++ Export_functions.py
@@ -1,11 +1,16 @@
-def access(Filesource):
+#Funktionsbibliothek für den Exporter
+#Ersteller: Tobias Dominik Weber
+#Datum: 01.04.2020 Versin 0.8
+
+#nimmt Pfadname entgegen und gibt True zurück, wenn die Datei existiert und nicht beschrieben wird
+def access(source): 
     
     import os
 
-    if os.path.exists(Filesource):       # testen ob Datei existiert
+    if os.path.exists(source):       # testen ob Datei existiert
 
         try:                           # testen ob Datei geändert werden kann
-            f = open(Filesource, "w")    
+            f = open(source, "w")    
             ergebnis = True            
             f.close() 
             
@@ -14,3 +19,29 @@ def access(Filesource):
 
         finally:
             return (ergebnis)
+
+
+def mail():
+    pass # Eingefügt, wenn ich es endlich zum laufen bekomme
+    """
+    import smtplib
+    from email.MIMEMultipart import MIMEMultipart
+    from email.MIMEText import MIMEText
+
+    senderEmail = "tobiasweber1029@googlemail.com"
+    empfangsEmail = "tobiasweber1029@googlemail.com"
+    msg = MIMEMultipart()
+    msg['From'] = senderEmail
+    msg['To'] = empfangsEmail
+    msg['Subject'] = "Es ist ein Fehler aufgetreten"
+
+    #emailText = "Das Programm wurde unerwartet beendet. Bitte neu starten!"
+    #msg.attach(MIMEText(emailText, 'html'))
+
+    server = smtplib.SMTP('mail.google.net', 993) # Die Server Daten
+    server.starttls()
+    server.login(senderEmail, "tobiasdominiki") # Das Passwort
+    text = msg.as_string()
+    server.sendmail(senderEmail, empfangsEmail, text)
+    server.quit()
+    """
