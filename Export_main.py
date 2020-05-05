2602a593056e1e9639afe587a665dde40994fb25
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-05-05 13:40:49 +0200

    Updated Design and a Bugfix in the Loop
    Fixed a Main-function call where i forgot to transfer the parameters

--- Export_main.py
+++ Export_main.py
@@ -11,14 +11,23 @@ wait = ""
 counter = 0
 
 Data_source = open('exporter_config.txt', 'r') #Nur die drei Angaben Quelle, Ziel und wartezeit einlesen
+
 for i in Data_source:  
+
     counter += 1
+
     if counter == 1:
+
         source = i
+
     if counter == 2:
+
         destination = i 
+
     if counter == 3:
+
         wait = i
+
 Data_source.close()
 
 source = source.lstrip('Quellpfad: â€ª ') #Pfad zurechtschneiden
@@ -27,30 +36,38 @@ source = source.strip()
 destination = destination.lstrip('Zielpfad: â€ª ') #Pfad zurechtschneiden
 destination = destination.strip()
 
-
 wait = int(wait.lstrip('Wartezeit in Sekunden: ')) #Auf Zeit reduzieren
-
-
 
 def main(source, destination, wait):
     try:
+
         for filename in os.listdir(source): # Jede Datei einzeln nacheinander abrufen 
+
             Filesource = source + filename
+
             if func.access(Filesource) == True: # testen, ob die datei existiert und man exklusiven Zugriff hat
 
                 shutil.copy((Filesource), (destination) + (filename)) # Datei kopieren
                 os.remove((source) + (filename)) # Alte Datei löschen
+
             else:
+
                 continue
+        
+        time.sleep(wait)
+        main(source, destination, wait)
+
     except FileNotFoundError: #später mail mit Warnung verschicken
+        
         print('Das Verzeichnis, das es abzuarbeiten gilt, wird nicht gefunden. Die Verarbeitung wird deshalb abgebrochen!')
         #input ('BITTE TASTE DRÜCKEN ZUM PROGRAMM BEENDEN!')       
-        func.mail() #WIP
-    
-    time.sleep(wait) #Wartet x Sekunden, bevor der Ordner wieder abgearbeitet wird
-    main()
-
+        #func.mail() #WIP
 
 if __name__ == "__main__":
+    
+    print("The CCB_File_Exporter is now running. Close this window to shut down the Programm!")
     main(source, destination, wait)
+
+print("An Error Occured. The Programm will shut down!")
+input()
 
