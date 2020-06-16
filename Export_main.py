2bf696601c067111e1c9849607a4ea5694409a17
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-06-16 12:39:25 +0200

    Updated the Design
    Now the Main-Programm is a loop of two files.
    
    checkSettings reads the Informations out of the config.
    It also creates a new config.txt if it isnt found.
    When all settings are extracted, it passes them to the main function.
    
    The Main function does all the work on checking and moving the files. After the Main is done, it calls the checkSettings function again to update, if the settings have been changed.

--- Export_main.py
+++ Export_main.py
@@ -3,71 +3,78 @@ import os
 import Export_functions as func
 import time
 
-#kann keine Ordner kopieren
 
-source = ""
-destination = ""
-wait = ""
-counter = 0
-
-Data_source = open('exporter_config.txt', 'r') #Nur die drei Angaben Quelle, Ziel und wartezeit einlesen
-
-for i in Data_source:  
-
-    counter += 1
-
-    if counter == 1:
-
-        source = i
-
-    if counter == 2:
-
-        destination = i 
-
-    if counter == 3:
-
-        wait = i
-
-Data_source.close()
-
-source = source.lstrip('Quellpfad: â€ª ') #Pfad zurechtschneiden
-source = source.strip()
-
-destination = destination.lstrip('Zielpfad: â€ª ') #Pfad zurechtschneiden
-destination = destination.strip()
-
-wait = int(wait.lstrip('Wartezeit in Sekunden: ')) #Auf Zeit reduzieren
-
-def main(source, destination, wait):
+# Extract the Settings from the exporter_config.txt File
+def checkSettings():
+    counter = 0
     try:
+        data_source = open("exporter_config.txt", "r") 
+        for i in data_source:  
+            counter += 1
+            if counter == 1:
+                source = i # source of Files
+                source = source.lstrip("Quellpfad: â€ª ") #extract only valid Information
+                source = source.strip()
+            if counter == 2:
+                destination = i # Destination for Files that match the format
+                destination = destination.lstrip("Zielpfad: â€ª ") #extract only valid Information
+                destination = destination.strip()
+            if counter == 3:
+                wrongDestination = i # Destination for Files that dont match the format
+                wrongDestination = wrongDestination.lstrip("Zielpfad, falls falscher Dateityp: â€ª ") #extract only valid Information
+                wrongDestination = wrongDestination.strip()
+            if counter == 4:
+                wait = i # Time to wait after each loop
+                wait = int(wait.lstrip("Wartezeit in Sekunden: ")) #extract only valid time
+            if counter == 5:
+                fileType = i # The ending to check if the File has the right Type
+                fileType = fileType.lstrip("Endug der gewünschten Dateityps: ") #extract only valid Information
+                fileType = fileType.strip()
+            if counter > 5:
+                break
+        data_source.close()
+        main(source, destination, wait, wrongDestination, fileType)
+            
+    except FileNotFoundError: # create new config.txt
+        file = open("exporter_config.txt", "w")
+        file.write("Quellpfad: ")
+        file.write("Zielpfad: ")
+        file.write("Zielpfad, falls falscher Dateityp: ")
+        file.write("Wartezeit in Sekunden: ")
+        file.write("Endug der gewünschten Dateityps: " + "\n")
+        file.write("""!WICHTIG!
+Nach dem Doppelpunkt muss immer ein Zeichen Platz sein. 
+Quell- und Zielpfad im folgenden Format angeben: C:/Users/Test/quelle/ oder C:/ziel/
+Der Pfadname darf keine "\" enthalten und muss am Schluss ein "/" stehen haben
+Die Wartezeit nur als Zahl angeben.
+Die Endung im Format z.B: .pdf .py .exe .docx """)
+        file.close()
+        time.sleep(300) # wait 5 Minutes for the User to Input settings
+        checkSettings()
 
-        for filename in os.listdir(source): # Jede Datei einzeln nacheinander abrufen 
 
-            Filesource = source + filename
-
-            if func.access(Filesource) == True: # testen, ob die datei existiert und man exklusiven Zugriff hat
-
-                shutil.copy((Filesource), (destination) + (filename)) # Datei kopieren
-                os.remove((source) + (filename)) # Alte Datei löschen
+def main(source, destination, wait, wrongDestination, fileType):
+    try:
+        for filename in os.listdir(source): 
+            filesource = source + filename
+            if func.access(filesource) == True: # check if the file exists and isnt opened
+                if func.isType(filename, fileType) == True: # check if the File has the right type
+                    shutil.move(filesource, destination)
+                else:
+                    shutil.move(filesource, wrongDestination)
 
             else:
-
                 continue
-        
-        time.sleep(wait)
-        main(source, destination, wait)
+    except FileNotFoundError: # send Mail with Error 
+        func.mail() #WIP
+    
+    time.sleep(wait) #Wait x Seconds, befor a new loop
+    checkSettings()
 
-    except FileNotFoundError: #später mail mit Warnung verschicken
-        
-        print('Das Verzeichnis, das es abzuarbeiten gilt, wird nicht gefunden. Die Verarbeitung wird deshalb abgebrochen!')
-        #input ('BITTE TASTE DRÜCKEN ZUM PROGRAMM BEENDEN!')       
-        #func.mail() #WIP
 
 if __name__ == "__main__":
-    
-    print("The CCB_File_Exporter is now running. Close this window to shut down the Programm!")
-    main(source, destination, wait)
+    checkSettings()
 
-print("An Error Occured. The Programm will shut down!")
-input()
+
+
 
