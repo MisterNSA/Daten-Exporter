1c01e03f3e98ca8e5680a14cdbb0475553e511ee
MisterNSA <53508377+MisterNSA@users.noreply.github.com>
2020-06-16 12:32:49 +0200

    added new functions
    added a function to determine, if the file is of the choosen datatype.

--- Export_functions.py
+++ Export_functions.py
@@ -1,28 +1,32 @@
-#Funktionsbibliothek für den Exporter
-#Ersteller: Tobias Dominik Weber
-#Datum: 01.04.2020 Versin 0.8
+#Funktionlibrary for the Exporter
+#Creator: Tobias Dominik Weber
+#Date: 15.06.2020 Versin 0.9
 
-#nimmt Pfadname entgegen und gibt True zurück, wenn die Datei existiert und nicht beschrieben wird
+
+# check if the File matches the Type from the config
+def isType(source, fileType):
+    if fileType in source:
+        return True
+    else:
+        return False 
+
+
+# check if the File exists and isnt open 
 def access(source): 
-    
     import os
-
-    if os.path.exists(source):       # testen ob Datei existiert
-
-        try:                           # testen ob Datei geändert werden kann
+    if os.path.exists(source):       # check if File existiert
+        try:                           # check if file is opened
             f = open(source, "w")    
             ergebnis = True            
             f.close() 
-            
         except:
             ergebnis = False
-
         finally:
-            return (ergebnis)
+            return ergebnis
 
 
 def mail():
-    pass # Eingefügt, wenn ich es endlich zum laufen bekomme
+    pass # WORK IN PROGRESS
     """
     import smtplib
     from email.MIMEMultipart import MIMEMultipart
@@ -31,14 +35,14 @@ def mail():
     senderEmail = "tobiasweber1029@googlemail.com"
     empfangsEmail = "tobiasweber1029@googlemail.com"
     msg = MIMEMultipart()
-    msg['From'] = senderEmail
-    msg['To'] = empfangsEmail
-    msg['Subject'] = "Es ist ein Fehler aufgetreten"
+    msg["From"] = senderEmail
+    msg["To"] = empfangsEmail
+    msg["Subject"] = "Es ist ein Fehler aufgetreten"
 
     #emailText = "Das Programm wurde unerwartet beendet. Bitte neu starten!"
-    #msg.attach(MIMEText(emailText, 'html'))
+    #msg.attach(MIMEText(emailText, "html"))
 
-    server = smtplib.SMTP('mail.google.net', 993) # Die Server Daten
+    server = smtplib.SMTP("mail.google.net", 993) # Die Server Daten
     server.starttls()
     server.login(senderEmail, "tobiasdominiki") # Das Passwort
     text = msg.as_string()
