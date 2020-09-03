6cbd15039d8b648bdf42b181831c1a63ed6dff7a
MisterNSA <tobiasweber1029@gmail.com>
2020-09-03 10:57:01 +0200

    Added function to validate filename

--- Export_functions.py
+++ Export_functions.py
@@ -1,17 +1,24 @@
 # Funktionlibrary for the Exporter
-# Creator: Tobias Dominik Weber
-# Date: 17.08.2020 Version 0.9.2
+# Creator: MisterNSA aka Tobias Dominik Weber
+# Date: 03.09.2020 Version 1.0.0
 
-# check if the File matches the Type from the config
 def isType(source, fileType):
+    """check if the File matches the Type from the config"""
     if any(ending in source for ending in fileType):
         return True
     else:
         return False
 
+def Starts_with_Number(filename):
+    """Check if the File starts with a number. Needed for assignment"""
+    try:
+        int(filename[0])
+        return True
+    except:
+        return False
 
-# check if the File exists and isnt open
 def access(source):
+    """check if the File exists and isnt open"""
     import os
     if os.path.exists(source):       
         try: # check if file is opened
@@ -24,8 +31,8 @@ def access(source):
             return ergebnis
 
 
-# sends a Mail with an error message to the user
 def mail(error_message, Address_sender, Password_sender, Address_receiver, Smtp_server):
+    """sends a Mail with an error message to the user"""
     import smtplib
     # SMTP Server and Port
     connectionObject = smtplib.SMTP(Smtp_server)
