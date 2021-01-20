9f7dad14094a16bd9dac62cfee41dba432d10a01
MisterNSA <tobiasweber1029@gmail.com>
2021-01-20 12:54:36 +0100

    Update Export_functions.py
    Mail nicht mehr benötigt.
    Zugriff wird nun in Main geprüft.

--- Export_functions.py
+++ Export_functions.py
@@ -3,11 +3,6 @@
 # Date: 03.09.2020 Version 1.0.0
 
 import os
-from dotenv import load_dotenv
-
-
-
-
 
 def isType(source, fileType):
     """check if the File matches the Type from the config"""
@@ -27,47 +22,9 @@ def Starts_with_Number(filename):
     except:
         return False
 
-def access(source):
-    """check if the File exists and isnt open"""
-    import os
-    # BUG: Tut nicht richtig, siehe TODO
-    # TODO: Die Funktion tut noch nicht genau das was sie tun soll !
-    # es wird nicht verhindert das z.B. eine geöffnete Access Datenbank, also eine geöffnete Datei
-    # eines anderen Prozesses, verschoben wird, und damit kommt es zu, Fehler !
-    # Das sollte diese Funktion eigentlich überprüfen !!!
-
-    if os.path.exists(source):       
-        try: # check if file is opened
-            f = open(source, "a+")
-            ergebnis = True
-            f.close()
-        except:
-            ergebnis = False
-        finally:
-            return ergebnis
-
-
-def mail(error_message):
-    """sends a Mail with an error message to the user"""
-    import smtplib
-    # SMTP Server and Port
-    connectionObject = smtplib.SMTP(os.getenv("SMTP_SERVER"))
-    # connect to SMTP
-    connectionObject.ehlo()
-    # Beginn Encryption
-    connectionObject.starttls()
-    # Login to your Account: USER | PW
-    connectionObject.login(os.getenv("ADDRESS_SENDER"), os.getenv("PASSWORD_SENDER"))
-    # From, To, Subject \n\n Text
-    connectionObject.sendmail(os.getenv("ADDRESS_SENDER"), os.getenv("ADDRESS_RECEIVER") ,
-                              f"Subject: An Error Occured\n\n Datum: {getTime()} \n {error_message} ")
-    # Disconnect
-    connectionObject.quit()
-
 def getTime():
     from datetime import datetime
     now = datetime.now()
     # Extract Date and Time
     return now.strftime("%Y.%m.%d - %H:%M:%S")
 
-#mail("Hilfe, es hat funktioniert")
